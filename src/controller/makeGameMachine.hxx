5973045bfe2f14177b354ad76e3f0a51fcc40864
werto87 <werto87>
2021-05-13 17:26:41 +0200

    chat grey out

--- src/controller/makeGameMachine.hxx
+++ src/controller/makeGameMachine.hxx
@@ -14,15 +14,16 @@ struct MakeGameMachine
         // clang-format off
         //TODO add statemachine which wraps this and deals with lobby events and with evalChat 
         //right now we have to write the same code again and again we can send an event and handle it in wrapper
-        // TODO add grey out when wait for server in chat
+        
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/      
 * state<Lobby>                        + event<lobbyWaitForServer>                                                                      = state<LobbyWaitForServer>
-, state<Lobby>                        + event<shared_class::JoinChannelSuccess>   / reactToJoinChannelSuccess
-, state<Lobby>                        + event<shared_class::Message>              / reactToMessage
-, state<Lobby>                        + event<draw>                               / (drawLobby,evalLobby,evalChat)         
+, state<Lobby>                        + event<draw>                               / (drawLobby,evalLobby,evalChatLobby)         
 , state<Lobby>                        + on_entry<_>                               / resetPopupAndWaitForServer
+, state<Lobby>                        + event<shared_class::Message>              / reactToMessage        
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/ 
 , state<LobbyWaitForServer>           + on_entry<_>                               / setLobbyWaitForServer
+, state<LobbyWaitForServer>           + event<shared_class::Message>              / reactToMessage                                     = state<Lobby>
+, state<LobbyWaitForServer>           + event<shared_class::JoinChannelSuccess>   / reactToJoinChannelSuccess                          = state<Lobby>
 , state<LobbyWaitForServer>           + event<shared_class::CreateGameLobbyError> / setErrorEvent                                                                      
 , state<LobbyWaitForServer>           + event<shared_class::JoinGameLobbyError>   / setErrorEvent
 , state<LobbyWaitForServer>           + event<shared_class::JoinGameLobbySuccess>                                                      = state<CreateGameLobby>
@@ -32,17 +33,19 @@ struct MakeGameMachine
 , state<CreateGameLobby>              + on_entry<_>                               / resetPopupAndWaitForServer
 , state<CreateGameLobby>              + event<createGameLobbyWaitForServer>                                                            = state<CreateGameLobbyWaitForServer>
 , state<CreateGameLobby>              + event<lobby>                                                                                   = state<Lobby>
-, state<CreateGameLobby>              + event<shared_class::JoinChannelSuccess>   / reactToJoinChannelSuccess
 , state<CreateGameLobby>              + event<shared_class::Message>              / reactToMessage
 , state<CreateGameLobby>              + event<shared_class::UsersInGameLobby>     / reactToUsersInGameLobby
-, state<CreateGameLobby>              + event<draw>                               / (drawCreateGameLobby,evalCreateGameLobby,evalChat)  
+, state<CreateGameLobby>              + event<draw>                               / (drawCreateGameLobby,evalCreateGameLobby,evalChatCreateGameLobby)  
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
-, state<CreateGameLobbyWaitForServer>           + on_entry<_>                                               / setCreateGameLobbyWaitForServer
-, state<CreateGameLobbyWaitForServer>           + event<shared_class::SetMaxUserSizeInCreateGameLobbyError> / setErrorEvent                                                                      
-, state<CreateGameLobbyWaitForServer>           + event<shared_class::MaxUserSizeInCreateGameLobby>                                                                 = state<CreateGameLobby>
-, state<CreateGameLobbyWaitForServer>           + event<lobby>                                                                                                      = state<Lobby>
-, state<CreateGameLobbyWaitForServer>           + event<draw>                                               / (drawCreateGameLobby,evalCreateGameLobbyWaitForServer)         
+, state<CreateGameLobbyWaitForServer> + on_entry<_>                                               / setCreateGameLobbyWaitForServer
+, state<CreateGameLobbyWaitForServer> + event<shared_class::Message>              / reactToMessage                                     = state<CreateGameLobby>
+, state<CreateGameLobbyWaitForServer> + event<shared_class::JoinChannelSuccess>   / reactToJoinChannelSuccess                          = state<CreateGameLobby>
+, state<CreateGameLobbyWaitForServer> + event<shared_class::SetMaxUserSizeInCreateGameLobbyError> / setErrorEvent                                                                      
+, state<CreateGameLobbyWaitForServer> + event<shared_class::MaxUserSizeInCreateGameLobby>                                                                 = state<CreateGameLobby>
+, state<CreateGameLobbyWaitForServer> + event<lobby>                                                                                                      = state<Lobby>
+, state<CreateGameLobbyWaitForServer> + event<draw>                                               / (drawCreateGameLobby,evalCreateGameLobbyWaitForServer)         
 /*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
 );
     // clang-format on 
   }
