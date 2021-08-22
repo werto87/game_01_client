12879516f711664ed4909714bcb152d33ac89cbe
werto87 <huhulasmichinruhe@web.de>
2021-08-22 15:35:47 +0200

    handle strange websocket url

--- main.cxx
+++ main.cxx
@@ -1,14 +1,9 @@
 #include "src/ui/ui.hxx"
+#include <Magnum/Platform/EmscriptenApplication.h>
+#include <Magnum/Platform/Sdl2Application.h>
 #include <boost/json/src.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/program_options/cmdline.hpp>
-#ifdef CORRADE_TARGET_ANDROID
-#include <Magnum/Platform/AndroidApplication.h>
-#elif defined(CORRADE_TARGET_EMSCRIPTEN)
-#include <Magnum/Platform/EmscriptenApplication.h>
-#else
-#include <Magnum/Platform/Sdl2Application.h>
-#endif
 #include <cxxopts.hpp>
 #include <iostream>
 
@@ -36,14 +31,16 @@ main (int argc, char **argv)
       std::cout << options.help () << std::endl;
       exit (0);
     }
-  std::cout << "after cxxopts" << std::endl;
-  std::cout << "argc: " << argc << std::endl;
-  std::copy (argv + 1, argv + argc, std::ostream_iterator<const char *> (std::cout, "\n"));
-  int tmpArgc = 1; /**< @brief Argument count */
-
+  int tmpArgc = 1;      // work around to ignore commandline input for magnum imgui
   char *tmpArgv[1];     // work around to ignore commandline input for magnum imgui
   tmpArgv[0] = argv[0]; // work around to ignore commandline input for magnum imgui
 
-  emscriptenApplicationInstance.reset (new ImGuiExample{ { tmpArgc, tmpArgv }, result["touch"].as<bool> (), result["websocket-url"].as<std::string> () });
+  auto websocketUrl = result["websocket-url"].as<std::string> ();
+  if (websocketUrl == "{{WEBSOCKET-URL}}")
+    {
+      websocketUrl = "wss://localhost:55555";
+      std::cout << "websocketUrl == \"{{WEBSOCKET-URL}}\" using " << websocketUrl << std::endl;
+    }
+  emscriptenApplicationInstance.reset (new ImGuiExample{ { tmpArgc, tmpArgv }, result["touch"].as<bool> (), websocketUrl });
   emscriptenApplicationInstance->redraw ();
 }
